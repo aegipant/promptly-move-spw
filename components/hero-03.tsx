c6c6cb8c0550914045e6433c3f394488e60d5ccd
Sincere TSANG <t.shing101@gmail.com>
2025-07-08 01:04:51 +0800

    development v20250708001

--- components/hero-03.tsx
+++ components/hero-03.tsx
@@ -13,7 +13,7 @@ const Hero03 = () => {
         </p>
       </div>
        <div className="mt-6 flex items-center justify-center gap-4">
-        <a href="http://wa.me/85254635464" target="_blank" rel="noopener noreferrer">
+        <a href="https://wa.me/85254635464" target="_blank" rel="noopener noreferrer">
           <Button
             variant="outline"
             size="lg"
@@ -35,8 +35,8 @@ const Hero03 = () => {
         </a>
       </div>
       <div className="flex">
-      <Image src="/banner.jpeg" width={800} height={800} alt="" /></div>
-        <div className="w-full md:w-1/2">即到提供一站式搬屋服務，包括女團 Pack 箱、裝修存倉、傢俬存倉、棄置傢俬等，無論村屋、公屋、居屋、住宅、辦公室、商舖，港九新界都幫到你。
+      <a href="https://wa.me/85254635464"><Image src="/banner.jpeg" width={800} height={800} alt="" /></a></div>
+        <div className="w-full md:w-1/2" style={{color:'white'}} >即到提供一站式搬屋服務，包括女團 Pack 箱、裝修存倉、傢俬存倉、棄置傢俬等，無論村屋、公屋、居屋、住宅、辦公室、商舖，港九新界都幫到你。
             即到上門儲存盒服務，專人免費上門派盒收盒，無需簽約，無需預繳，無需按金，支援多種電子付款方式。
             想搵一個方便快捷、無隱藏收費、更幫你上門收送的專業存倉搬屋公司? 即到搬屋公司就是你的不二之選！</div>
       <Link href="#section2Features">
